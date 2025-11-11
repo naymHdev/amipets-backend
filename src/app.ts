85ab3a64e40a07a7af84f793b60848c847418af2
naymHdev <naym100m@gmail.com>
2025-11-11 15:39:10 +0600

    update size

--- src/app.ts
+++ src/app.ts
@@ -38,9 +38,11 @@ app.use(
 export const httpServer = createServer(app);
 socketIo(httpServer);
 
+
+
 app.use(cookieParser());
-app.use(express.json({limit : "50mb"}));
-app.use(express.urlencoded({ extended: true, limit : "50mb" }));
+app.use(express.json({limit : "1000mb"}))
+app.use(express.urlencoded({ extended: true, limit : "1000mb", parameterLimit: 1000000 }));
 app.use(express.static('public'));
 
 app.use('/api/v1', router);
