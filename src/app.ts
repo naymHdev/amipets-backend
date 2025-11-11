afce6fa26ed44481c65f357a3484c5adc4bc0e27
naymHdev <naym100m@gmail.com>
2025-11-11 15:21:29 +0600

    added request limit 50mb from default 1 mb

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ export const httpServer = createServer(app);
 socketIo(httpServer);
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({limit : "50mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
